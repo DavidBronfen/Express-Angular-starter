f4729b775253186457b9ec5a03f4bd6b449c1d77
David Bronfen <davidbronfen@gmail.com>
2019-06-25 15:30:38 +0300

    Add client dependencies (#4)
    * Added style pre processor options.
    
    * Added dependencies.
    
    * Import all dependencies in app module.
    
    * Added variable style file.
    
    * Added font and icon link to index file.
    
    * Upgrade angular to last version.
    
    * Added angular cdk library.
    
    * Remove app component data.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatIconModule,
+} from '@angular/material';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +22,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    FlexLayoutModule,
+    // StoreModule.forRoot(),
+    // EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
